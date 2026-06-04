40fbeba4b927830c9af2c934bcf3462b4f17e965
YOLO0378 <03sun0708@gmail.com>
2026-06-04 12:49:02 +0900

    Add files via upload
    dto(미완성), dao(미완성), 한영변환(미완성)과 관련된 파일을 업로드 하였다.

---
+++ src/main/java/dto/chatroom.java
@@ -0,0 +1,65 @@
+package dto;
+
+import java.io.Serializable;
+
+public class chatroom implements Serializable {
+		private static final long serialVersionUID=-4274700572038677000L;
+		
+		private String roomId;
+		private String roomname;
+		private String description;
+		private int entry;
+		private String filename;
+		
+		public chatroom() {
+			super();
+		}
+		
+		public chatroom(String roomId, String roomname, int entry) {
+			this.roomId=roomId;
+			this.roomname=roomname;
+			this.entry = entry;
+			this.roomId=roomId;
+		}
+
+		public String getRoomId() {
+			return roomId;
+		}
+
+		public void setRoomId(String roomId) {
+			this.roomId = roomId;
+		}
+
+		public String getRoomname() {
+			return roomname;
+		}
+
+		public void setRoomname(String roomname) {
+			this.roomname = roomname;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public int getEntry() {
+			return entry;
+		}
+
+		public void setEntry(int entry) {
+			this.entry = entry;
+		}
+
+		public String getFilename() {
+			return filename;
+		}
+
+		public void setFilename(String filename) {
+			this.filename = filename;
+		}
+
+}
