656e4005e205b0b886349e452499d516a9837708
hokagn <184084617+hokagn@users.noreply.github.com>
2026-06-08 01:33:30 +0900

    룸 리포지터리에 싱글톤 패턴 적용

--- src/main/java/dao/RoomRepository.java
+++ src/main/java/dao/RoomRepository.java
@@ -7,13 +7,22 @@ public class RoomRepository {
 	
 	private ArrayList<chatroom> listOfChatrooms = new ArrayList<chatroom>();
 
-	public RoomRepository() {
+	private RoomRepository() {
 		chatroom chatroom1 = new chatroom("rm0000001", "전체 체팅방", 0);
 		chatroom1.setDescription("최초로 생성되는 채팅방이다.");
 		chatroom1.setFilename("default.jpg");
 		
 		listOfChatrooms.add(chatroom1);
 		
+	}
+	private static RoomRepository singleton = null;
+	public static RoomRepository get()
+	{
+		if(singleton == null)
+		{
+			singleton = new RoomRepository();
+		}
+		return singleton;
 	}
 	public ArrayList<chatroom> getAllChatrooms(){
 		return	listOfChatrooms;
